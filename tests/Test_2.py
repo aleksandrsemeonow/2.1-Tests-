fb793627e1dcd4d07a5daf3c43c638b75589534a
aleksandrsemeonow <49782093+aleksandrsemeonow@users.noreply.github.com>
2020-01-20 17:15:32 +0500

    Update Test_2.py

--- tests/Test_2.py
+++ tests/Test_2.py
@@ -1,11 +1,28 @@
-from Task_2 import translate
+import requests
 import unittest
+import yadisk
 
+API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
+URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+token = 'AgAAAAAH_QAQAAXkX7xLlzVVVE9HhDkQ57m_j1k'
+y = yadisk.YaDisk (token = token)
+
+def translate_it(text, to_lang):
+    params = {
+        'key': API_KEY,
+        'text': text,
+        'lang': to_lang,
+
+    }
+
+    response = requests.get (URL, params = params)
+    return response
 
 class TestGetRequest(unittest.TestCase):
 
     def right_translate(self):
-        self.assertEqual('привет', translate.translate_it('Hello', 'ru'))
+        result = translate_it('Hello', 'ru')
+        self.assertEqual('привет', result)
 
 if __name__ == '__main__':
     unittest.main()
