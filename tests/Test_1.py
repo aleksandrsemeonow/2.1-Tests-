aed334e7061ef9fcf4b5f7c3ddc61e3d29c5dcf6
aleksandrsemeonow <49782093+aleksandrsemeonow@users.noreply.github.com>
2020-01-21 14:28:46 +0500

    Update Test_1.py

--- tests/Test_1.py
+++ tests/Test_1.py
@@ -1,8 +1,12 @@
 import unittest
-from Task_1 import function
+from unittest.mock import patch
+from Task_1 import main
 from Task_1.directories import directories
 from Task_1.documents import documents
 
+
+@patch('Task_1.main.documents', documents, create=True)
+@patch('Task_1.main.directories', directories, create=True)
 
 class TestGetDocuments(unittest.TestCase):
 
@@ -12,15 +16,15 @@ class TestGetDocuments(unittest.TestCase):
 
     def test_add_doc_on_directories(self):
         len_doc = len(self.directories['2'])
-        function.add_docs('505050', 'snils', '2')
+        main.add_docs('505050', 'snils', '2')
         self.assertGreater(len(self.directories['2']), len_doc)
 
     def test_find_docs(self):
-        self.assertEqual('Василий Гупкин', function.people('2207 876234'))
+        self.assertEqual('Василий Гупкин', main.people('2207 876234'))
 
     def test_delete_doc_from_directories(self):
         len_doc = len(self.directories['1'])
-        function.delete_doc('2207 876234')
+        main.delete_doc('2207 876234')
         self.assertLess(len(self.directories['1']), len_doc)
 
 if __name__ == '__main__':
