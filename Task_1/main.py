9f1206d7ffe3ed45daf5900bac9d17fa1ee0bc63
aleksandrsemeonow <49782093+aleksandrsemeonow@users.noreply.github.com>
2020-01-22 12:22:20 +0500

    Update and rename function.py to main.py

--- Task_1/main.py
+++ Task_1/main.py
@@ -1,19 +1,21 @@
-from Task_1.directories import directories
-from Task_1.documents import documents
+from .documents import documents
+from .directories import directories
 
 def people(user_input_people):
-  for n in documents:
-    if n["number"] == user_input_people:
-      return (n['name'])
-    else:
-      return 'Такого документа нет!'
+    for doc in documents:
+        number_of_doc = doc['number']
+        if number_of_doc == user_input_people:
+            return doc['name']
+
+
 
 def delete_doc(number_of_doc):
-  for doc in documents:
-    if doc['number'] == number_of_doc:
-      documents.remove(doc)
-      remove_doc_from_shelf(number_of_doc)
-      return documents, directories
+    for doc in documents:
+        if doc['number'] == number_of_doc:
+            documents.remove(doc)
+            remove_doc_from_shelf(number_of_doc)
+            return documents, directories
+
 
 def remove_doc_from_shelf(number_of_doc):
     for number_of_shelf, list_of_docs in directories.items():
@@ -22,33 +24,33 @@ def remove_doc_from_shelf(number_of_doc):
 
 
 def show_docs():
-  for list_docs in documents:
-    x = list_docs['type']
-    y = list_docs['number']
-    z = list_docs['name']
-    print(x,y,z)
-
+    for list_docs in documents:
+        x = list_docs['type']
+        y = list_docs['number']
+        z = list_docs['name']
+        print(x, y, z)
 
 
 def shelf():
-  user_input_shelf = input (str ('Введите номер документа: '))
-  for list_dir, list_docs in directories.items ():
-    for list_docs_val in list_docs:
-      if user_input_shelf == list_docs_val:
-        directories_1 =  (list_dir)
-        return directories_1
-  else:
-    return('Такого документа нет')
+    user_input_shelf = input(str('Введите номер документа: '))
+    for list_dir, list_docs in directories.items():
+        for list_docs_val in list_docs:
+            if user_input_shelf == list_docs_val:
+                directories_1 = (list_dir)
+                return directories_1
+    else:
+        return ('Такого документа нет')
 
 
 def add_docs(user_input_number, user_input_type, user_input_directories):
-  documents.append ({'type': user_input_type, 'number': user_input_number})
-  if user_input_directories in directories.keys():
-    directories[user_input_directories].append(user_input_number)
-    return directories
-  if user_input_directories not in directories.keys():
-    directories[user_input_directories] = [user_input_number]
-    return directories
+    documents.append({'type': user_input_type, 'number': user_input_number})
+    if user_input_directories in directories.keys():
+        directories[user_input_directories].append(user_input_number)
+        return directories
+    if user_input_directories not in directories.keys():
+        directories[user_input_directories] = [user_input_number]
+        return directories
+
 
 def commands():
     user_input = input(str('\nВведите одну из комманд:'
@@ -62,14 +64,12 @@ def commands():
                            ' и в перечень полок,'
                            ' спросив его номер, тип, имя владельца и номер полки, на котором он будет храниться.\n'))
     if user_input == 'p':
-      print(people())
+        print(people())
     elif user_input == 'l':
-      show_docs()
+        show_docs()
     elif user_input == 's':
-      print(shelf())
+        print(shelf())
     elif user_input == 'a':
-      print(add_docs(user_input_number=int(input()), user_input_type=input(), user_input_directories=int(input())))
+        print(add_docs(user_input_number=int(input()), user_input_type=input(), user_input_directories=int(input())))
     else:
-      print('Такой команды нет!')
-
-
+        print('Такой команды нет!')
